fdc7c51bcf96875dd7737b85319ee71b5ae5f8c4
zia-hussain <syedziashahgillani76@gmail.com>
2023-11-02 03:39:49 -0700

    Final Commit

--- app/components/TicTacToe.jsx
+++ app/components/TicTacToe.jsx
@@ -1,134 +1,136 @@
-"use client"
-import { useState , useEffect} from "react";
+"use client";
+import { useState, useEffect } from "react";
 import Board from "./Board";
 import GameOver from "./GameOver";
 import GameState from "./GameState";
 import Reset from "./Reset";
-import gameOverSoundAsset from '../Sounds/game_over.wav';
-import clickSoundAsset from '../Sounds/click.wav';
+import gameOverSoundAsset from "../Sounds/game_over.mp3";
+import clickSoundAsset from "../Sounds/click.mp3";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
+// console.log(clickSoundAsset, typeof clickSoundAsset)
+// const gameOverSound = new Audio(gameOverSoundAsset);
+// gameOverSound.volume = 0.2;
+// const clickSound = new Audio(clickSoundAsset);
+// clickSound.volume = 0.5;
 
-console.log(clickSoundAsset, typeof clickSoundAsset)
-const gameOverSound = new Audio(gameOverSoundAsset);
-gameOverSound.volume = 0.2;
-const clickSound = new Audio(clickSoundAsset);
-clickSound.volume = 0.5;
-
-
-const PLAYER_X = 'X';
-const PLAYER_0 = 'O';
+const PLAYER_X = "X";
+const PLAYER_0 = "O";
 
 const winningCombinations = [
-    //Rows
-    {combo:[0,1,2] ,strikeClass:"strike-row-1"},
-    {combo:[3,4,5] ,strikeClass:"strike-row-2"},
-    {combo:[6,7,8] ,strikeClass:"strike-row-3"},
+  //Rows
+  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
+  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
 
-    //Columns
-    {combo:[0,3,6] ,strikeClass:"strike-column-1"},
-    {combo:[1,4,7] ,strikeClass:"strike-column-2"},
-    {combo:[2,5,8] ,strikeClass:"strike-column-3"},
+  //Columns
+  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
 
-    //diagonals
+  //diagonals
 
-    {combo:[0,4,8] ,strikeClass:"strike-diagonal-1"},
-    {combo:[2,4,6] ,strikeClass:"strike-diagonal-2"},
-
+  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
 ];
 
+function checkWinner(tiles, setStrikeClass, setGameState) {
+  for (const { combo, strikeClass } of winningCombinations) {
+    const tileValue1 = tiles[combo[0]];
+    const tileValue2 = tiles[combo[1]];
+    const tileValue3 = tiles[combo[2]];
 
-
-function checkWinner(tiles,setStrikeClass , setGameState){
-    for(const {combo, strikeClass} of winningCombinations) {
-        const tileValue1 = tiles[combo[0]];
-        const tileValue2 = tiles[combo[1]];
-        const tileValue3 = tiles[combo[2]];
-
-        if(tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3)
-        {
-            setStrikeClass(strikeClass);
-            if(tileValue1 === PLAYER_X){
-                setGameState(GameState.playerXWins)
-
-            }
-            else{
-                setGameState(GameState.playerOWins)
-            }
-            return ;
-        }
-    };
-
-    const areAllTilesFilledIn = tiles.every((tile)=> tile !== null);
-    if(areAllTilesFilledIn) {
-        setGameState(GameState.draw);
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      setStrikeClass(strikeClass);
+      if (tileValue1 === PLAYER_X) {
+        setGameState(GameState.playerXWins);
+      } else {
+        setGameState(GameState.playerOWins);
+      }
+      return;
     }
-};
+  }
 
+  const areAllTilesFilledIn = tiles.every((tile) => tile !== null);
+  if (areAllTilesFilledIn) {
+    setGameState(GameState.draw);
+  }
+}
 
-function TicTacToe(){
-    const [tiles,setTiles] = useState(Array(9).fill(null));
-    const [playerTurn,setPlayerTurn] = useState(PLAYER_X);
-    const[strikeClass,setStrikeClass] = useState();
-    const[gameState,setGameState] = useState(GameState.inProgress)
+function TicTacToe() {
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+  const [strikeClass, setStrikeClass] = useState();
+  const [gameState, setGameState] = useState(GameState.inProgress);
 
-    const HandleTileClick = (index)=>{
-        if(gameState!== GameState.inProgress) {
-            return ;
-        }
+  const HandleTileClick = (index) => {
+    if (gameState !== GameState.inProgress) {
+      return;
+    }
 
-        if(tiles[index] !== null){
-            return;
-        }
-        const newTiles = [...tiles];
-        newTiles[index] = playerTurn;
-        setTiles(newTiles);
-        if(playerTurn === PLAYER_X) {
-            setPlayerTurn(PLAYER_0);
-        } else {
-            setPlayerTurn(PLAYER_X)
-        }
-    };
+    if (tiles[index] !== null) {
+      return;
+    }
+    const newTiles = [...tiles];
+    newTiles[index] = playerTurn;
+    setTiles(newTiles);
+    if (playerTurn === PLAYER_X) {
+      setPlayerTurn(PLAYER_0);
+    } else {
+      setPlayerTurn(PLAYER_X);
+    }
+  };
 
-    const handleReset = ()=>{
+  const handleReset = () => {
     setGameState(GameState.inProgress);
     setTiles(Array(9).fill(null));
     setPlayerTurn(PLAYER_X);
     setStrikeClass(null);
-       
-    };
+  };
 
+  useEffect(() => {
+    checkWinner(tiles, setStrikeClass, setGameState);
+  }, [tiles]);
 
-    useEffect(()=>{
-        checkWinner(tiles,setStrikeClass , setGameState);
-    }, [tiles]);
+  useEffect(() => {
+    if (tiles.some((tile) => tile !== null)) {
+      const clickSound = new Audio(clickSoundAsset);
+      clickSound.volume = 0.2;
+      clickSound.play();
+    }
+  }, [tiles]);
 
-
-    useEffect(() => {
-        if (tiles.some((tile) => tile !== null)) {
-            console.log("Condition met");
-            console.log(clickSound)
-          clickSound.play();
-        }
-      }, [tiles]);
-
-    useEffect(() => {
-        if (gameState !== GameState.inProgress) {
-            console.log("Game over");
-
-          gameOverSound.play();
-        }
-      }, [gameState]);
-
-
-    return (
-        <div>
-            <h1>Tic Tac Toe</h1>
-            <Board playerTurn ={playerTurn} tiles = {tiles}
-             onTileClick={HandleTileClick} 
-             strikeClass={strikeClass}
-             />
-           <GameOver gameState={gameState} />
-           <Reset gameState={gameState} onReset={handleReset}/>
-        </div>
-    );
+  useEffect(() => {
+    if (gameState !== GameState.inProgress) {
+      const gameOverSound = new Audio(gameOverSoundAsset);
+      gameOverSound.volume = 0.5;
+      gameOverSound.play();
+    }
+  }, [gameState]);
+  const { width, height } = useWindowSize();
+  return (
+    <div>
+      {gameState === 1 || gameState === 0 ? (
+        <>
+          <Confetti width={width} height={height} />
+        </>
+      ) : (
+        <></>
+      )}
+      <h1>Tic Tac Toe</h1>
+      <Board
+        playerTurn={playerTurn}
+        tiles={tiles}
+        onTileClick={HandleTileClick}
+        strikeClass={strikeClass}
+      />
+      <GameOver gameState={gameState} />
+      <Reset gameState={gameState} onReset={handleReset} />
+    </div>
+  );
 }
 export default TicTacToe;
